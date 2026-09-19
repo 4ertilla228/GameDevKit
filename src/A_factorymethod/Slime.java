4e81d8b192b3a37974b709f6fab86f1ece546d14
Alan Baiguzhinov <252858@astanait.edu.kz>
2026-09-19 15:16:44 +0500

    конкретный класс продукт. указали папку, добавили аннотацию атаки, чтоб не было ошибок

---
+++ src/A_factorymethod/Slime.java
@@ -0,0 +1,8 @@
+package A_factorymethod;
+
+public class Slime implements Enemy {
+    @Override
+    public void attack() {
+        System.out.println("Слайм плюется кислотной слизью!");
+    }
+}
