3c1e836385eafd4599f38ca490373360e5b79c60
Alan Baiguzhinov <252858@astanait.edu.kz>
2026-09-19 15:15:55 +0500

    конкретный класс продукт. указали папку, добавили аннотацию атаки, чтоб не было ошибок

---
+++ src/A_factorymethod/Skeleton.java
@@ -0,0 +1,8 @@
+package A_factorymethod;
+
+public class Skeleton implements Enemy {
+    @Override
+    public void attack() {
+        System.out.println("Скелет стреляет костяной стрелой из лука!");
+    }
+}
