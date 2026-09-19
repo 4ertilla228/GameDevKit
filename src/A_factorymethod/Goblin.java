05f7d5bd947ef7536fe0767fb806172a183a4d45
Alan Baiguzhinov <252858@astanait.edu.kz>
2026-09-19 15:14:14 +0500

    конкретный класс продукт. указали папку, добавили аннотацию атаки, чтоб не было ошибок

---
+++ src/A_factorymethod/Goblin.java
@@ -0,0 +1,8 @@
+package A_factorymethod;
+
+public class Goblin implements Enemy {
+    @Override
+    public void attack() {
+        System.out.println("Гоблин с криком размахивает дубиной!");
+    }
+}
