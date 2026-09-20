b1c889af6b2ec1bdbfed4822ad679629e7b27492
Alan Baiguzhinov <252858@astanait.edu.kz>
2026-09-20 15:54:03 +0500

    .

--- src/A_factorymethod/EnemySpawner.java
+++ src/A_factorymethod/EnemySpawner.java
@@ -8,7 +8,7 @@ public abstract class EnemySpawner {
     // бизнес
     public void spawnAndAttack() {
         System.out.println("——— Система спавна активирована ———");
-        // делегируем создание объекта фабричному методу
+        // передаём задачи создание объекта фабричному методу
         Enemy enemy = createEnemy();
         // используем продукт
         enemy.attack();
